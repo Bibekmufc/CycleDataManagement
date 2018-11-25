a889be85b2c7c62204ae7d8d71ffe154b8a1f3a4
Bibek Gautam <bibekmufc21@gmail.com>
2018-11-25 20:32:49 +0545

    Added Calculate Class; Revised Code

--- CycleDataManagement/Form1.Designer.cs
+++ CycleDataManagement/Form1.Designer.cs
@@ -30,21 +30,319 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblavhrate = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lblmaxhrate = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lblavpwr = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lblmaxpwr = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lblmaxspeed = new System.Windows.Forms.Label();
+            this.lblavalt = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lblavspeed = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbltotal = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblstar = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbldate = new System.Windows.Forms.Label();
+            this.lblinterval = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.viewDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Open = new System.Windows.Forms.ToolStripMenuItem();
             this.viewGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.panel2.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.splitContainer1);
             this.panel1.Controls.Add(this.menuStrip1);
             this.panel1.Location = new System.Drawing.Point(3, 6);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1095, 643);
             this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel3);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGrid);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(1095, 615);
+            this.splitContainer1.SplitterDistance = 835;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Controls.Add(this.lblavhrate);
+            this.panel3.Controls.Add(this.lbl4);
+            this.panel3.Controls.Add(this.lblmaxhrate);
+            this.panel3.Controls.Add(this.lbl5);
+            this.panel3.Controls.Add(this.lblavpwr);
+            this.panel3.Controls.Add(this.lbl6);
+            this.panel3.Controls.Add(this.lblmaxpwr);
+            this.panel3.Controls.Add(this.lbl7);
+            this.panel3.Controls.Add(this.lblmaxspeed);
+            this.panel3.Controls.Add(this.lblavalt);
+            this.panel3.Controls.Add(this.lbl8);
+            this.panel3.Controls.Add(this.lblavspeed);
+            this.panel3.Controls.Add(this.lbl3);
+            this.panel3.Controls.Add(this.lbltotal);
+            this.panel3.Controls.Add(this.lbl2);
+            this.panel3.Controls.Add(this.lbl1);
+            this.panel3.Location = new System.Drawing.Point(0, 351);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(832, 261);
+            this.panel3.TabIndex = 1;
+            // 
+            // lblavhrate
+            // 
+            this.lblavhrate.AutoSize = true;
+            this.lblavhrate.Location = new System.Drawing.Point(239, 138);
+            this.lblavhrate.Name = "lblavhrate";
+            this.lblavhrate.Size = new System.Drawing.Size(31, 17);
+            this.lblavhrate.TabIndex = 10;
+            this.lblavhrate.Text = "N/A";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(33, 138);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(174, 17);
+            this.lbl4.TabIndex = 10;
+            this.lbl4.Text = "Average Heartbeat Rate : ";
+            // 
+            // lblmaxhrate
+            // 
+            this.lblmaxhrate.AutoSize = true;
+            this.lblmaxhrate.Location = new System.Drawing.Point(241, 168);
+            this.lblmaxhrate.Name = "lblmaxhrate";
+            this.lblmaxhrate.Size = new System.Drawing.Size(31, 17);
+            this.lblmaxhrate.TabIndex = 9;
+            this.lblmaxhrate.Text = "N/A";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(35, 168);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(146, 17);
+            this.lbl5.TabIndex = 9;
+            this.lbl5.Text = "Max Heartbeat Rate : ";
+            // 
+            // lblavpwr
+            // 
+            this.lblavpwr.AutoSize = true;
+            this.lblavpwr.Location = new System.Drawing.Point(241, 197);
+            this.lblavpwr.Name = "lblavpwr";
+            this.lblavpwr.Size = new System.Drawing.Size(31, 17);
+            this.lblavpwr.TabIndex = 8;
+            this.lblavpwr.Text = "N/A";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(35, 197);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(116, 17);
+            this.lbl6.TabIndex = 8;
+            this.lbl6.Text = "Average Power : ";
+            // 
+            // lblmaxpwr
+            // 
+            this.lblmaxpwr.AutoSize = true;
+            this.lblmaxpwr.Location = new System.Drawing.Point(623, 43);
+            this.lblmaxpwr.Name = "lblmaxpwr";
+            this.lblmaxpwr.Size = new System.Drawing.Size(31, 17);
+            this.lblmaxpwr.TabIndex = 7;
+            this.lblmaxpwr.Text = "N/A";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(462, 43);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(88, 17);
+            this.lbl7.TabIndex = 7;
+            this.lbl7.Text = "Max Power : ";
+            // 
+            // lblmaxspeed
+            // 
+            this.lblmaxspeed.AutoSize = true;
+            this.lblmaxspeed.Location = new System.Drawing.Point(241, 106);
+            this.lblmaxspeed.Name = "lblmaxspeed";
+            this.lblmaxspeed.Size = new System.Drawing.Size(31, 17);
+            this.lblmaxspeed.TabIndex = 3;
+            this.lblmaxspeed.Text = "N/A";
+            // 
+            // lblavalt
+            // 
+            this.lblavalt.AutoSize = true;
+            this.lblavalt.Location = new System.Drawing.Point(623, 76);
+            this.lblavalt.Name = "lblavalt";
+            this.lblavalt.Size = new System.Drawing.Size(31, 17);
+            this.lblavalt.TabIndex = 4;
+            this.lblavalt.Text = "N/A";
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.Location = new System.Drawing.Point(462, 76);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(124, 17);
+            this.lbl8.TabIndex = 4;
+            this.lbl8.Text = "Average Altitude : ";
+            // 
+            // lblavspeed
+            // 
+            this.lblavspeed.AutoSize = true;
+            this.lblavspeed.Location = new System.Drawing.Point(238, 76);
+            this.lblavspeed.Name = "lblavspeed";
+            this.lblavspeed.Size = new System.Drawing.Size(31, 17);
+            this.lblavspeed.TabIndex = 2;
+            this.lblavspeed.Text = "N/A";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(35, 106);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(123, 17);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "Maximum Speed : ";
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(238, 43);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(31, 17);
+            this.lbltotal.TabIndex = 1;
+            this.lbltotal.Text = "N/A";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(32, 76);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(118, 17);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Average Speed : ";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(32, 43);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(111, 17);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Total Distance : ";
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(0, 0);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(832, 345);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.lblstar);
+            this.panel2.Controls.Add(this.lbl11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.lbldate);
+            this.panel2.Controls.Add(this.lblinterval);
+            this.panel2.Controls.Add(this.lbl9);
+            this.panel2.Location = new System.Drawing.Point(23, 192);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(207, 219);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblstar
+            // 
+            this.lblstar.AutoSize = true;
+            this.lblstar.Location = new System.Drawing.Point(118, 84);
+            this.lblstar.Name = "lblstar";
+            this.lblstar.Size = new System.Drawing.Size(31, 17);
+            this.lblstar.TabIndex = 3;
+            this.lblstar.Text = "N/A";
+            // 
+            // lbl11
+            // 
+            this.lbl11.AutoSize = true;
+            this.lbl11.Location = new System.Drawing.Point(29, 84);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(85, 17);
+            this.lbl11.TabIndex = 3;
+            this.lbl11.Text = "Start Time : ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(29, 136);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 17);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Interval : ";
+            // 
+            // lbldate
+            // 
+            this.lbldate.AutoSize = true;
+            this.lbldate.Location = new System.Drawing.Point(118, 37);
+            this.lbldate.Name = "lbldate";
+            this.lbldate.Size = new System.Drawing.Size(31, 17);
+            this.lbldate.TabIndex = 0;
+            this.lbldate.Text = "N/A";
+            // 
+            // lblinterval
+            // 
+            this.lblinterval.AutoSize = true;
+            this.lblinterval.Location = new System.Drawing.Point(118, 136);
+            this.lblinterval.Name = "lblinterval";
+            this.lblinterval.Size = new System.Drawing.Size(31, 17);
+            this.lblinterval.TabIndex = 2;
+            this.lblinterval.Text = "N/A";
+            // 
+            // lbl9
+            // 
+            this.lbl9.AutoSize = true;
+            this.lbl9.Location = new System.Drawing.Point(29, 37);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(50, 17);
+            this.lbl9.TabIndex = 0;
+            this.lbl9.Text = "Date : ";
             // 
             // menuStrip1
             // 
@@ -60,9 +358,18 @@
             // 
             // viewDataToolStripMenuItem
             // 
+            this.viewDataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Open});
             this.viewDataToolStripMenuItem.Name = "viewDataToolStripMenuItem";
             this.viewDataToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
             this.viewDataToolStripMenuItem.Text = "View Data";
+            // 
+            // Open
+            // 
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(216, 26);
+            this.Open.Text = "Open File";
+            this.Open.Click += new System.EventHandler(this.Open_Click);
             // 
             // viewGraphToolStripMenuItem
             // 
@@ -87,6 +394,15 @@
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -100,6 +416,33 @@
         private System.Windows.Forms.ToolStripMenuItem viewDataToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewGraphToolStripMenuItem;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label lblinterval;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lblavhrate;
+        private System.Windows.Forms.Label lblmaxhrate;
+        private System.Windows.Forms.Label lblavpwr;
+        private System.Windows.Forms.Label lblmaxpwr;
+        private System.Windows.Forms.Label lblmaxspeed;
+        private System.Windows.Forms.Label lblavalt;
+        private System.Windows.Forms.Label lblavspeed;
+        private System.Windows.Forms.Label lbltotal;
+        private System.Windows.Forms.Label lblstar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbldate;
+        private System.Windows.Forms.ToolStripMenuItem Open;
     }
 }
 
--- CycleDataManagement/Form1.cs
+++ CycleDataManagement/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,199 @@ namespace CycleDataManagement
 {
     public partial class Form1 : Form
     {
+        public Dictionary<string, List<string>> Data = new Dictionary<string, List<string>>();
+        private Dictionary<string, string> parameter = new Dictionary<string, string>();
+        public List<string> paraArrays = new List<string>();
+        public List<string> speed = new List<string>();
+        public List<string> cadence = new List<string>();
+        public List<string> altitude = new List<string>();
+        public List<string> power = new List<string>();
+        public List<string> heartRate = new List<string>();
+        public List<string> powerBalancePedalling = new List<string>();
+        int counter = 0;
+        char[] findOf = { '\t', ' ', '=' };
+
+
         public Form1()
         {
             InitializeComponent();
+            InitializeGrid();
+        }
+
+        //specifying column header
+        private void InitializeGrid()
+        {
+            dataGrid.ColumnCount = 5;
+            dataGrid.Columns[0].Name = "Heart Rate (bpm)";
+            dataGrid.Columns[1].Name = "Speed (km/h)";
+            dataGrid.Columns[2].Name = "Cadence (rpm)";
+            dataGrid.Columns[3].Name = "Altitude (m/ft)";
+            dataGrid.Columns[4].Name = "Power (watts)";
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            dataGrid.DataSource = null;
+            dataGrid.Rows.Clear();
+            ReadSource();
+            viewData();
+            Calc();
+        }
+
+        //read and display the data from  file
+        public void ReadSource()
+        {
+            string line = "";
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "HRM|*.hrm|Text Document|*.txt";
+            op.Title = "Open File";
+            // Display the file select dialog box.  
+            // If the user clicked OK in the dialog then  ,
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = (FileStream)op.OpenFile();
+                StreamReader sr = new StreamReader(fs);
+
+                while (!(line = sr.ReadLine()).Contains("[Note]"))
+
+                {
+                    counter++;
+
+                    string newline = string.Join(" ", line.Split(findOf, StringSplitOptions.RemoveEmptyEntries));
+                    List<string> val = newline.Split(' ').ToList();
+
+                    for (int i = 0; i < val.Count(); i++)
+                    {
+                        paraArrays.Add(val[i]);
+                    }
+
+                }
+                try
+                {
+                    for (int i = 1; i < paraArrays.Count(); i += 2)
+                    {
+                        parameter.Add(paraArrays[i], paraArrays[0 + i]);
+                    }
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                while (!sr.EndOfStream)
+                {
+                    if ((line = sr.ReadLine()).Contains("[HRData]"))
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            //add specific type data to array 
+                            sortDataToArray(line);
+                        }
+                    }
+                }
+
+                //adding hr data to dictionary for easy access
+                DataDictionary();
+
+
+                if (parameter.ContainsKey("Date"))
+                {
+                    string date = parameter["Date"];
+                    DateTime dt = DateTime.ParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                    lbldate.Text = dt.ToString("dddd, MMMM dd yyyy");
+                }
+                if (parameter.ContainsKey("StartTime"))
+                {
+                    string time = parameter["StartTime"];
+                    lblstar.Text = time;
+                }
+                if (parameter.ContainsKey("Interval"))
+                {
+                    string intval = parameter["Interval"];
+                    lblinterval.Text = intval;
+                }
+
+
+            }
+        }
+        //split data and adding into list array
+        public void sortDataToArray(string line)
+        {
+            try
+            {
+
+                string newline = string.Join(" ", line.Split(findOf, StringSplitOptions.RemoveEmptyEntries));
+                List<string> val = newline.Split(' ').ToList();
+                heartRate.Add(val[0]);
+                speed.Add(val[1]);
+                cadence.Add(val[2]);
+                altitude.Add(val[3]);
+                power.Add(val[4]);
+                powerBalancePedalling.Add(val[5]);
+                val = null;
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //displayed list data to grid view table
+        public void viewData()
+        {
+
+            int counter = 0;
+            foreach (var value in speed)
+            {
+                dataGrid.Rows.Add(heartRate[counter]
+                    , speed[counter]
+                    , cadence[counter]
+                    , altitude[counter]
+                    , power[counter]);
+                counter++;
+            }
+        }
+
+        //adding HRdata to dictionary 
+        private void DataDictionary()
+        {
+            Data.Add("heartRate", heartRate);
+            Data.Add("speed", speed);
+            Data.Add("cadence", cadence);
+            Data.Add("altitude", altitude);
+            Data.Add("power", power);
+        }
+
+
+        //summary calculation
+        private void Calc()
+        {
+            var maxSpeed = Calculate.Max(Data["speed"]);
+            //var totalDistanceCovered = Summary.Sum(hrData["cadence"]);
+            var averageSpeed = Calculate.Average(Data["speed"]);
+            var averageHeartRate = Calculate.Average(Data["heartRate"]);
+            var maximumHeartRate = Calculate.Max(Data["heartRate"]);
+            var minimumHeartRate = Calculate.Min(Data["heartRate"]);
+
+            var averagePower = Calculate.Average(Data["power"]);
+            var maximumPower = Calculate.Max(Data["power"]);
+
+            var averageAltitude = Calculate.Average(Data["altitude"]);
+            var maximumAltitude = Calculate.Max(Data["altitude"]);
+
+            //summary of data 
+            lbltotal.Text = null;
+            lblavspeed.Text = averageSpeed.ToString();
+            lblmaxspeed.Text = maxSpeed.ToString();
+            lblavhrate.Text = averageHeartRate.ToString();
+            lblmaxhrate.Text = maximumHeartRate.ToString();
+            lblavhrate.Text = minimumHeartRate.ToString();
+            lblavpwr.Text = averagePower.ToString();
+            lblmaxpwr.Text = maximumPower.ToString();
+            lblavalt.Text = averageAltitude.ToString();
+
+
         }
     }
 }
